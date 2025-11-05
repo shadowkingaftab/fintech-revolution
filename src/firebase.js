69a7d84621beef203e8050b1496837e9637a3903
shadowkingaftab <aftab.sayed.0044@gmail.com>
2025-11-06 01:24:38 +0530

    Add Vercel domain

--- src/firebase.js
+++ src/firebase.js
@@ -3,7 +3,6 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-// YOUR CONFIG FROM FIREBASE (YOU ALREADY HAVE THIS)
 const firebaseConfig = {
   apiKey: "AIzaSyAOFidaYTaipufy-igQ5k-zBLqJ0J_UCio",
   authDomain: "fintech-revolution.firebaseapp.com",
@@ -13,10 +12,11 @@ const firebaseConfig = {
   appId: "1:380406848205:web:72583cea072ac718732784"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// EXPORT THESE — THIS IS WHAT WAS MISSING
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
+googleProvider.setCustomParameters({
+  prompt: 'select_account'
+});
 export const db = getFirestore(app);
